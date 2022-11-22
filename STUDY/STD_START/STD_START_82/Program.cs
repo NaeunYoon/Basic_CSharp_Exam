f71cc05bd4d6e4d0ed03c4498346c0a5923ef0a3
NaeunYoon <whalebaby@naver.com>
2022-11-23 07:58:49 +0900

    Retry

---
+++ STUDY/STD_START/STD_START_82/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STD_START_82
+{
+    internal class Program
+    {
+        class Nothing
+        {
+            int a;
+
+            public int A
+            {
+                get { return a; }
+                set { a = value; }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            short a = 1;
+            int b = 2;
+            //b = a;
+            a = (short)b;
+            Console.WriteLine(a);
+
+            if (b is int)
+            {
+                Console.WriteLine("b는 string");
+            }
+
+            ACLASS A=new ACLASS();
+            BCLASS B = A as BCLASS;
+            if (B == null)
+            {
+                Console.WriteLine("실패");
+            }
+        }
+
+        class ACLASS
+        {
+
+        }
+        class BCLASS : ACLASS
+        {
+
+        }
+    }
+}
